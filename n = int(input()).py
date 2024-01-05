d378851013d15d8749c10b41a10174a39dff1e9a
Vitaly Tananykin <98489171+tvgVita69@users.noreply.github.com>
2024-01-05 18:08:06 +0300

    Add files via upload

---
+++ n = int(input()).py
@@ -0,0 +1,11 @@
+n = int(input())
+if n ==0 or n > 12:
+    print('NO')
+elif n == 1 or n == 12 or n == 2 :
+    rint('winter')
+elif n == 3 or n == 4 or n == 5 :
+    print('spring')
+elif n == 6 or n == 7 or n == 8 :
+    print('summer')
+elif n == 9 or n == 10 or n == 11 :
+    print('autumn')
